0fb25b238b5d4d33106d170e45a94bb4fecbbcb2
Emerson Rocha de Oliveira <emersonrochadeoliveira@gmail.com>
2018-05-06 03:00:55 -0300

    de volta pra 3000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const watson = require('watson-developer-cloud');
 const express = require('express');
 
 const app = express();
-//const port = 5000;
+const port = 3000;
 
 app.use(express.static('./public'));
 
