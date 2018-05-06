551565a05e8a409999644e5148c54b357028767a
Emerson Rocha de Oliveira <emersonrochadeoliveira@gmail.com>
2018-05-06 02:27:08 -0300

    ajustes

---
+++ public/js/negociacao.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('#div-negociacao div a').click(function(){
+        $('#div-negociacao').addClass("hidden");
+        // var id = $(this).attr('id');
+        // alert(id);
+    });
+});
