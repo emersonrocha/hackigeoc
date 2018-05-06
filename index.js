859095fbc288b14159d6833db067f79fa4c51e7d
Emerson Rocha de Oliveira <emersonrochadeoliveira@gmail.com>
2018-05-06 04:42:21 -0300

    8080

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const watson = require('watson-developer-cloud');
 const express = require('express');
 
 const app = express();
-const port = 80;
+const port = 8080;
 
 app.use(express.static('./public'));
 
