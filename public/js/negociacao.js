df4ffe1d3f57f1fe26e6745d9a1947a2551af859
Emerson Rocha de Oliveira <emersonrochadeoliveira@gmail.com>
2018-05-06 04:00:50 -0300

    Ajustes

--- public/js/negociacao.js
+++ public/js/negociacao.js
@@ -1,6 +1,16 @@
 $(function(){
     $('#div-negociacao div a').click(function(){
+        $('#div-login').removeClass("hidden");
         $('#div-negociacao').addClass("hidden");
+        $('#div-login').addClass("visible");
+        // var id = $(this).attr('id');
+        // alert(id);
+    });
+
+    $('#div-login div a').click(function(){
+        $('#div-user').removeClass("hidden");
+        $('#div-login').addClass("hidden");
+        $('#div-user').addClass("visible");
         // var id = $(this).attr('id');
         // alert(id);
     });
